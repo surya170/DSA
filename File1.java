81e3b5c1a787d4b24494ca0e39603b054656eacd
Surya <suryanarayana13271@gmail.com>
2021-12-07 12:15:24 +0530

    Write The Code OtpActivity()

--- File1.java
+++ File1.java
@@ -2,6 +2,44 @@ class  Main
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("The App Execution start here");
 	}
+	public static void OTPActivity(Long number) {
+          return 0;
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
