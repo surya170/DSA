07e6d8d2887c7cc7055ca2f53b78ca721d31710b
Surya <suryanarayana13271@gmail.com>
2021-12-08 09:53:44 +0530

    Numerical Pattern coding is solved

--- Fundamentals/src/org/ar/pattern/pattern10.java
+++ Fundamentals/src/org/ar/pattern/pattern10.java
@@ -1,10 +1,25 @@
 package org.ar.pattern;
 
+import java.util.Scanner;
+
 public class pattern10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+       Scanner sc = new Scanner(System.in);
+       int n  = sc.nextInt();
+       int count = 1;
+       for(int r=1;r<=n;r++) {
+    	   char ch  = 'A';
+    	   for(int c=1;c<=r;c++) {
+    		  System.out.print(ch+" ");
+    		  ch++;
+    	   }
+    	   ch--;
+    	   for(int c=1;c<=r-1;c++) {
+    		   System.out.print(--ch+" ");
+    	   }
+    	   System.out.println();
+       }
 	}
 
 }
